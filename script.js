0c4592e1c850207ff3498cfc1ac79ad1b74e511a
Melisa Lachev <melisatormac55@gmail.com>
2025-06-28 00:53:22 +0300

    dynamically calculated gap between hourly forecast lists on clicking btns

--- script.js
+++ script.js
@@ -16,3 +16,24 @@ const changeMode = function () {
 };
 
 switchBtn.addEventListener("click", changeMode);
+
+const rightBtn = document.querySelector(`.btn--right`);
+const lefttBtn = document.querySelector(`.btn--left`);
+
+const hourlyGap =
+  +window
+    .getComputedStyle(document.querySelector(`.forecast-list`))
+    .gap.replace(/\D/g, "") +
+  document.querySelector(`.hourly-forecast`).offsetWidth;
+
+const changeRightList = function () {
+  document.querySelector(`.forecast-list`).scrollLeft += hourlyGap;
+};
+
+console.log();
+
+const changeLeftList = function () {
+  document.querySelector(`.forecast-list`).scrollLeft -= hourlyGap;
+};
+rightBtn.addEventListener(`click`, changeRightList);
+lefttBtn.addEventListener(`click`, changeLeftList);
