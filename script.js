3088341f386b9ca218e6639c781cdab78766f4aa
Melisa Lachev <melisatormac55@gmail.com>
2025-07-01 19:48:04 +0300

    fixed dark and light mode
    added content to light and dark mode to show which mode is active

--- script.js
+++ script.js
@@ -13,14 +13,11 @@ const html = document.querySelector(`html`);
 
 const changeMode = function () {
   const data = html.getAttribute(`data-theme`);
-  console.log(data);
   if (data === `light`) {
     html.setAttribute("data-theme", `dark`);
-    console.log(`went thru light`);
     return;
   } else if (data === `dark`) {
     html.setAttribute("data-theme", `light`);
-    console.log(`went thru dark`);
     return;
   }
 };
