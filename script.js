8a31eee2399914b203853e53f74e7b1e6da2b544
Melisa Lachev <melisatormac55@gmail.com>
2025-07-01 18:25:35 +0300

    fixed css

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 import { API_KEY } from "./config.js";
 // API
 
-const weatherData = await fetch (`https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=sofia&days=3`)
-const data = await weatherData.json()
-console.log( data);
+const weatherData = await fetch(
+  `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=sofia&days=3`
+);
+const data = await weatherData.json();
+console.log(data);
 // NIGHT MODE
 
 const switchBtn = document.querySelector("span.slider");
